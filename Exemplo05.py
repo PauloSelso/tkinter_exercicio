64012f3fb397d28545ffcc3a1cdf629aab911acb
PauloSelso <cardepaulo@gmail.com>
2025-07-15 10:06:51 -0300

    Update Exemplo05.py

--- Exemplo05.py
+++ Exemplo05.py
@@ -4,4 +4,8 @@ class MinhaAPP:
     def _init_(self, parent):
         self.myParent = parent
         self.buttons_frame = tk.Frame(parent,background="lightgray") 
-        self.buttons_
+        self.buttons_frame.pack(padx=10, fill=tk.BOTH,expand=TRUE) 
+
+        self.botao1 =tk.Button(self.buttons_frame, text=
+
+        

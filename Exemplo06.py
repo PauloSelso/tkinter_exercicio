ea716ff3c4dffd05dcb96de52bc0a13375800669
PauloSelso <cardepaulo@gmail.com>
2025-07-15 11:48:21 -0300

    Create Exemplo06.py

---
+++ Exemplo06.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+class MinhaApp: 
+    def_init_(sef, parent):
+       self.myParent =parent
+       self.frame = tk.Frame(parent) 
+       self.frame.pack(padx=10, padx=10 
+
+       tk.Label(self.frame, text="Nome:", font=("Noto",12)).grid(row=0, column=0, padx=5 sticky="e") 
+       self.entrada_nome = tk.Entry(self.frame, font=("Noto", 12)) 
+       self.entrada_nome.grid(row=0, coluna=1, padx=5, pady=5 
+
+       tk.Label(self.frame, text="Idade:", font=("Noto",12)).grid(row=1, column=0
+                                                 
+
+                                                                    
